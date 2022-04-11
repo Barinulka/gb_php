4850942abe3f32431124f1b7503f4761a830779a
Barinulka <barinulka@yandex.ru>
2022-04-11 08:07:13 +0400

    lesson_5

---
+++ lesson_5/index.php
@@ -0,0 +1,52 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+     <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+
+     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
+     <title>Gallery</title>
+</head>
+<body>
+
+    <div class="container">
+        <div class="upload">
+            <div class="row">
+                <div class="col-12">
+                    <h2>Добавить фото</h2>
+                    <form action="config/upload.php" enctype="multipart/form-data" method="post"> 
+                        <input name="picture" type="file" />
+                        <input type="submit" value="Загрузить" />
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <div class="gallery">
+            <? include "gallery.php"; ?>
+        </div>
+    </div>
+
+
+    <div class="popup">
+        <div class="block">
+            <img class="image" src="" alt="">
+            <a class="close" href="#">x</a>
+        </div>
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <script src="js/script.js"></script>
+</body>
+</html>
