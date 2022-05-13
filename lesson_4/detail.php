3f1f6a23d9b1ea93709cb34b92c1df62db20b39a
Barinulka <barinulka@yandex.ru>
2022-05-13 10:37:37 +0400

    add db dump

--- lesson_4/detail.php
+++
@@ -1,7 +0,0 @@
-<?php
-if($_GET['name']){?>
-    <img width="600" src="img/<?= $_GET['name']?>" alt="">
-<?php
-}
-?>
-<a href="<?= $_SERVER['HTTP_REFERER']?>">Вернуться назад</a>
--- lesson_5/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-const SERVER = "localhost";
-const DB = "my_gallery";
-const LOGIN = "root";
-const PASS = "";
-
-$connect = mysqli_connect(SERVER,LOGIN,PASS,DB);
